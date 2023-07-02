1a1bb91a406710ab7b7547dad09a24ad5d2b723a
Ankush523 <ankush.ad050203@gmail.com>
2023-07-02 13:05:32 +0530

    added stackup paymaster for transactions

--- packages/snap/src/getSmartAccount.ts
+++ packages/snap/src/getSmartAccount.ts
@@ -4,6 +4,17 @@ import { Presets } from "userop";
 const entryPointAddress = '0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789';
 const factoryAddress = '0x9406Cc6185a346906296840746125a0E44976454';
 const rpcUrl ='https://api.stackup.sh/v1/node/30f261fef450031ac169889e4fc638d4526de1808ae25bd0c6d4609a323d671c';
+const paymasterUrl = "https://api.stackup.sh/v1/paymaster/30f261fef450031ac169889e4fc638d4526de1808ae25bd0c6d4609a323d671c"
+
+const paymaster = {
+    rpcUrl: paymasterUrl,
+    context: { "type": "payg" }
+}
+
+const paymasterMiddleware = Presets.Middleware.verifyingPaymaster(
+    paymaster.rpcUrl,
+    paymaster.context
+)
 
 export const getSmartAccount = async() => {
     const pvt_key = await snap.request({
@@ -17,6 +28,7 @@ export const getSmartAccount = async() => {
     const simpleAccount = await Presets.Builder.SimpleAccount.init(
         new ethers.Wallet(pvt_key),
         rpcUrl,
+        {paymasterMiddleware},
     )
 
     const address = simpleAccount.getSender();
--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -2,19 +2,34 @@ import { OnRpcRequestHandler } from '@metamask/snaps-types';
 import { heading, panel, text } from '@metamask/snaps-ui';
 import { getSmartAccount } from './getSmartAccount';
 import { Client, Presets } from "userop";
+import { ethers } from 'ethers';
 
 const aa_transact = async() => {
   const rpcUrl ='https://api.stackup.sh/v1/node/30f261fef450031ac169889e4fc638d4526de1808ae25bd0c6d4609a323d671c';
   const simpleAcc = await getSmartAccount();
   const client = await Client.init(rpcUrl);
+  
+  const walletAddress : any = await snap.request({
+    method: 'snap_dialog',
+    params: {
+      type: 'prompt',
+      content: panel([
+        heading('What is the wallet address?'),
+        text('Please enter the wallet address to be monitored'),
+      ]),
+      placeholder: '0x123...',
+    },
+  });
+
+  
+  const target = ethers.utils.getAddress(walletAddress);
+  const value = ethers.utils.parseEther("0.0001");
   const aaTx = await client.sendUserOperation(
-    simpleAcc.execute("0x1e87f3F4FDBb276250fC064a3cf0069592280601",10000,"0x"),
+    simpleAcc.execute(target,value,"0x"),
     { onBuild: (op) => console.log("Signed UserOperation:", op) }
   );
-  const userOpHash = aaTx.userOpHash
-  console.log("UserOperation Hash:", userOpHash);
-  const ev = await aaTx.wait();
-  return userOpHash;
+  return aaTx;
+
 }
 
 export const onRpcRequest: OnRpcRequestHandler = async({ origin, request }) => {
@@ -49,14 +64,19 @@ export const onRpcRequest: OnRpcRequestHandler = async({ origin, request }) => {
       });
     
     case 'sendAATx':
-      const txhash = await aa_transact();
+      const aaTxn = await aa_transact();
+      const userOpHash = aaTxn.userOpHash
+      const ev = await aaTxn.wait();
+      const txHash : any = ev?.transactionHash;
       return snap.request({
         method: 'snap_dialog',
         params: {
           type: 'confirmation',
           content: panel([
-            text(`Your transaction hash is :`),
-            text(txhash),
+            heading(`Your UserOP hash is :`),
+            text(userOpHash),
+            heading(`Your Transaction hash is :`),
+            text(txHash)
           ]),
         },
       });
